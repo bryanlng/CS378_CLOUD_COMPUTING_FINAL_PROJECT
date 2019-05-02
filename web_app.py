02171930aa865c192bd3f23e77c5e568449fdb37
bryan <bleungtx2@gmail.com>
2019-05-02 16:12:23 -0500

    finished the youtube data part of validation

--- web_app.py
+++ web_app.py
@@ -23,12 +23,12 @@ Takes in a url in the form of a string, then does the following:
 
 """
 def validate_youtube_url(url):
+    #Validate hostname. Check if it is a youtube url
     data = urlparse(url)
     youtube_share_urls = ["youtu.be", "www.youtu.be"]
     youtube_reg_urls = ["www.youtube.com", "youtube.com"]
     acceptable_hostnames = youtube_share_urls + youtube_reg_urls
 
-    #Validate hostname. Check if it is a youtube url
     hostname = str(data.hostname).lower()
     if(hostname is None or hostname not in acceptable_hostnames):
         print("bad")
@@ -43,16 +43,27 @@ def validate_youtube_url(url):
             if part[0:2] == "v=":
                 video_id = part[2:]
 
-    #Check if video has been blocked, by querying Youtube's Data API
+    #Check if video has been blocked, by querying Youtube's Data API videos api
     print("video_id:{}".format(video_id))
     youtube_query = "https://www.googleapis.com/youtube/v3/videos?part=id&id=" + video_id + "&key=" + YOUTUBE_DATA_API_V3
-    response = requests.get(youtube_query)
-    pretty_data = json.dumps(response.json(), indent=4)
-    print(pretty_data)
 
+    try:
+        raw = requests.get(youtube_query)
+        if raw.status_code == 400:
+            print("videoChart not found")
+        elif raw.status_code == 403:
+            print("forbidden")
+        elif raw.status_code == 404:
+            print("video not found")
+        else:
+            response = raw.json()
+            pretty_data = json.dumps(raw.json(), indent=4)
+            print(pretty_data)
 
-
-
+            if len(response["items"]) == 0:
+                print("video deleted")
+    except Exception as e:
+        print(e)
 
 
 
@@ -64,7 +75,5 @@ def hello():
 if __name__ == "__main__":
     app.debug = True
     test_url = "https://www.youtube.com/watch?v=URNN-_az-3g"
-    test_url = "https://youtu.be/URNN-_az-3g?t=40"
-    test_url = "https://youtu.be/40BNY41SJCI"
     validate_youtube_url(test_url)
     app.run(host='0.0.0.0')
