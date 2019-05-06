201e2d122bbe22d3c452cc6ef380bb494736af7e
bryan <bleungtx2@gmail.com>
2019-05-06 13:27:36 -0500

    jesus fucking christ it works, we CAN write files to a temporary directory called /tmp/. But don't include the title of it. Just make it be /tmp, not /tmp+yt.title

--- CloudFunctions.py
+++ CloudFunctions.py
@@ -3,8 +3,9 @@ from flask import escape
 from flask import jsonify
 
 from pytube import YouTube
-import youtube_dl
+# import youtube_dl
 import traceback
+import os
 
 import sys
 
@@ -24,11 +25,18 @@ def download_video(request):
     info = {}
     s = "dummy"
     info["dummy"] = s
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    cwd = os.getcwd()
+    info["dir_path"] = dir_path
+    info["cwd"] = cwd
     try:
         yt = YouTube(url)
         streams = yt.streams
-        info["streams"] = str(streams)
-        result = yt.streams.first().download()
+        # streams_list = []
+        # for stream in streams:
+        #     streams_list.append(stream)
+        # info["streams"] = str(streams_list)
+        result = yt.streams.first().download("/tmp")
 
     except Exception as e:
         error = traceback.print_exc()
@@ -40,41 +48,41 @@ def download_video(request):
 
 
 
-def download_video_youtube_dl(request):
-    """
-    Calls youtube-dl
-
-    Input:
-    return jsonify(request.args)
-    request object, which is a Flask Request object
-    Extract the following parameters out from request.args:
-    1. URL:
-        -the full url, not just the video id
-    """
-
-    # raw = str(request.args["url"])
-    # encoded = raw.encode("utf-8")
-    # url = io.BytesIO(encoded)
-
-    url = str(request.args.get("url"))
-    info = {}
-    # info["type"] = str(type(url))
-    # info["python_version"] = str(sys.version)
-    # info["Version_info"] = str(sys.version_info)
-    # info["url"] = str(url)
-    s = "dummy"
-    info["dummy"] = s
-    try:
-        ydl_opts = {
-            'outtmpl': '/tmp/%(id)s'
-        }
-        ydl_opts = {}
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            result = ydl.download([url])
-
-    except Exception as e:
-        error = traceback.print_exc()
-        info["problems_traceback"] = str(error)
-        info["problems_just_e"] = str(e)
-
-    return jsonify(info)
+# def download_video_youtube_dl(request):
+#     """
+#     Calls youtube-dl
+#
+#     Input:
+#     return jsonify(request.args)
+#     request object, which is a Flask Request object
+#     Extract the following parameters out from request.args:
+#     1. URL:
+#         -the full url, not just the video id
+#     """
+#
+#     # raw = str(request.args["url"])
+#     # encoded = raw.encode("utf-8")
+#     # url = io.BytesIO(encoded)
+#
+#     url = str(request.args.get("url"))
+#     info = {}
+#     # info["type"] = str(type(url))
+#     # info["python_version"] = str(sys.version)
+#     # info["Version_info"] = str(sys.version_info)
+#     # info["url"] = str(url)
+#     s = "dummy"
+#     info["dummy"] = s
+#     try:
+#         ydl_opts = {
+#             'outtmpl': '/tmp/%(id)s'
+#         }
+#         ydl_opts = {}
+#         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#             result = ydl.download([url])
+#
+#     except Exception as e:
+#         error = traceback.print_exc()
+#         info["problems_traceback"] = str(error)
+#         info["problems_just_e"] = str(e)
+#
+#     return jsonify(info)
