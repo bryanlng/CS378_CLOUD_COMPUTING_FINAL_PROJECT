187ab274460118c65acd077a48f1401e1c889e73
bryan <bleungtx2@gmail.com>
2019-05-06 01:10:52 -0500

    ok so it seems like it works with python 2.7 but not 3.7...., but the cloud function environment needs to be 3.7:

--- CloudFunctions.py
+++ CloudFunctions.py
@@ -2,10 +2,10 @@ from __future__ import unicode_literals
 from flask import escape
 from flask import jsonify
 
-from io import BytesIO
 import youtube_dl
 import traceback
 
+import sys
 
 
 
@@ -25,18 +25,23 @@ def download_video(request):
     # raw = str(request.args["url"])
     # encoded = raw.encode("utf-8")
     # url = io.BytesIO(encoded)
-    url = str(request.args["url"])
+
+    url = str(request.args.get("url"))
     info = {}
+    # info["type"] = str(type(url))
+    # info["python_version"] = str(sys.version)
+    # info["Version_info"] = str(sys.version_info)
+    # info["url"] = str(url)
+    s = "dummy"
+    info["dummy"] = s
     try:
         ydl_opts = {
             'outtmpl': '/tmp/%(id)s'
         }
         ydl_opts = {}
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            result = ydl.download(str('https://www.youtube.com/watch?v=dP15zlyra3c'))
+            result = ydl.download([url])
 
-
-        info["url"] = url
     except Exception as e:
         error = traceback.print_exc()
         info["problems_traceback"] = str(error)
